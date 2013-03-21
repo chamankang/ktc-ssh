d81736f7a16b8999e29d670ad2fae42cc6b2f9b0
Jesse Nelson <spheromak@gmail.com>
2013-03-21 02:20:51 -0700

    fixup auth keys, bump version and deps

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ site :opscode
 metadata
 
 cookbook "users", github: "cloudware-cookbooks/users"
+cookbook "helpers", github: "cloudware-cookbooks/helpers"
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email "spheromak@gmail.com"
 license          "Apache 2"
 description      "Cloudware SSH Wrapper Cook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.1.1"
 
 depends "openssh"
 depends "users"
+depends "helpers"
