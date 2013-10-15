0d07c0c538565b2081a67f5b211196839dbde419
Jesse Nelson <spheromak@gmail.com>
2013-10-14 22:48:52 -0700

    bump version, update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email "spheromak@gmail.com"
 license          "Apache 2"
 description      "Cloudware SSH Wrapper Cook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.3"
+version          "2.0.1"
 
-depends "helpers"
 depends "openssh"
 depends "users"
