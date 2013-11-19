b47a8f4361f317375eaf3583396dbd49a3edc9c9
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-11-19 16:46:05 +0900

    auto_version to 2.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "spheromak@gmail.com"
 license          "Apache 2"
 description      "Cloudware SSH Wrapper Cook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.1"
+version '2.0.2'
 
 depends "openssh"
 depends "users"
